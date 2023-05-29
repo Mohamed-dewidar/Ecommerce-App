0268a7ac5eae4921333d81c1c541a821104433b6
AmrAbrazek <amr.3brazek@gmail.com>
2023-05-29 16:04:08 +0300

    installing axios and import in indexjs

---
+++ e-commerce/src/components/admin/Mynav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+export default function Mynav() {
+
+  let navigate = useNavigate()
+
+
+  return (
+    <div className='my-3'>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"><NavLink className="nav-link" to="/home">ECOM</NavLink ></Navbar.Brand>
+          <Nav className="ms-auto">
+            <NavLink className="nav-link" to="/home">Home</NavLink >
+            <NavLink className="nav-link" to="/products">products</NavLink >
+            {/* <NavLink className="nav-link" to="/product/3">productdetails</NavLink > */}
+            <NavLink className="nav-link" to="/products/0/edit">Add products</NavLink >
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
